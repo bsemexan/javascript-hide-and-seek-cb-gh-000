4cca9d04d810227a4a18979a902748fdfa6a5e9d
Berry  Semexan <bsemexan@gmail.com>
2018-04-23 23:12:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector){
+  var first = document.querySelector(selector)
+  return first
+}
+
+function nestedTarget(){
+  var nested = document.querySelector('#nested .target')
+  return nested
+}
+
+function deepestChild(){
+  var deepest = document.querySelector('#grand-node div div div div')
+  return deepest
+}
+
+function increaseRankBy(n){
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0;  i < rankedLists.length; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
