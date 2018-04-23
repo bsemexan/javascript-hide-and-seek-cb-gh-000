3a4c12926ad10f52a22d13c3b69c258d0ae49814
Berry  Semexan <bsemexan@gmail.com>
2018-04-23 23:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function increaseRankBy(n){
   for (let i = 0,  i < rankedLists.length; i++) {
     let children = rankedLists[i].children
 
-    for (let j = 0, k = children.length; j < k; j++) {
+    for (let j = 0, j < children.length; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
